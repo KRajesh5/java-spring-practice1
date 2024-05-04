71b337b8cb1efcce84e668602820f71e4a0dc2a5
CG-FS-RAJE <rajeshkp41438@gmail.com>
2024-05-04 13:51:26 +0530

    first commit

---
+++ src/main/java/com/example/zipkin/JavaSpringPractice1Application.java
@@ -0,0 +1,13 @@
+package com.example.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringPractice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringPractice1Application.class, args);
+	}
+
+}
